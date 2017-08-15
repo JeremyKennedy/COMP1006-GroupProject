cec58eeb13945e0018fd54c7b82c5fa5e7a2b5da
Jeremy Kennedy <me@jeremyk.net>
2017-08-14 20:47:19 -0400

    make BadPlayer properly pick up cards until they have a valid move (instead of passing right away)

--- BadPlayer.java
+++ BadPlayer.java
@@ -10,25 +10,25 @@ public class BadPlayer extends Player {
 
 	/* play a card */
 	public boolean play(DiscardPile discardPile, Stack<Card> drawPile, ArrayList<Player> players) {
-
 		// go through all cards in the hand, and play the first valid one
-		for (int i = 0; i < hand.size(); i++) {
-			if (Objects.equals(hand.get(i).rank, discardPile.top().rank) ||
-					Objects.equals(hand.get(i).suit, discardPile.top().suit) ||
-					Objects.equals(hand.get(i).rank, Card.RANKS[8])) {
-				discardPile.add(this.hand.remove(i));
+		while (true) {
+			for (int i = 0; i < hand.size(); i++) {
+				if (Objects.equals(hand.get(i).rank, discardPile.top().rank) ||
+						Objects.equals(hand.get(i).suit, discardPile.top().suit) ||
+						Objects.equals(hand.get(i).rank, Card.RANKS[8])) {
+					discardPile.add(this.hand.remove(i));
+					return this.hand.size() == 0;
+				}
+			}
+
+			// pick up a card from the draw pile, if it isn't empty
+			if (!drawPile.isEmpty()) {
+				pickupCard(drawPile);
+			} else {
+				// nothing left to do, so pass
+				System.out.println("Passing!");
 				return this.hand.size() == 0;
 			}
 		}
-
-		// pick up a card from the draw pile, if it isn't empty
-		if (!drawPile.isEmpty()) {
-			pickupCard(drawPile);
-			return false;
-		}
-
-		// nothing left to do, so pass
-		System.out.println("Passing!");
-		return this.hand.size() == 0;
 	}
 }
