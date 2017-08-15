16c56c739416e93244bbe1d773d7e4cfb5549245
Jeremy Kennedy <me@jeremyk.net>
2017-08-14 20:08:49 -0400

    variable player types and counts

--- Crazy8Game.java
+++ Crazy8Game.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Stack;
 
 public class Crazy8Game {
 
 	public static void main(String[] args) {
-		
+		int BAD_PLAYERS = 3;     // BadPlayer
+		int RANDOM_PLAYERS = 1;  // RandomPlayer
+		int EIGHTS_PLAYERS = 1;  // MindTheEights
+		int HAMPER_PLAYERS = 1;  // HamperLeader
+		int HIGH_PLAYERS = 1;    // DiscardHighPoints
+		int EXTRA_PLAYERS = 1;   // ExtraCards
+
+
 		/* create the deck */
 		Card[] deck = new Card[52];
 		int index = 0;
@@ -25,22 +31,38 @@ public class Crazy8Game {
 			deck[pos] = deck[i];
 			deck[i] = swap;
 		}		
-		
-		/* players in the game */
-		ArrayList<Player> players = new ArrayList<>();
-		players.add(new BadPlayer(Arrays.copyOfRange(deck, 0, 5)));
-		System.out.println("Player 0's hand: " + players.get(0));
-		players.add(new BadPlayer(Arrays.copyOfRange(deck, 5, 10)));
-		System.out.println("Player 1's hand: " + players.get(1));
-		players.add(new BadPlayer(Arrays.copyOfRange(deck, 10, 15)));
-		System.out.println("Player 2's hand: " + players.get(2));
-		
-		
+
 		/* discard and draw piles */
 		DiscardPile discardPile = new DiscardPile();
 		Stack<Card> drawPile = new Stack<>();
-		for (int i = 15; i < deck.length; i++) {
+		for (int i = 0; i < deck.length; i++) {
 			drawPile.push(deck[i]);
+		}
+		deck = null;
+
+		/* players in the game */
+		ArrayList<Player> players = new ArrayList<>();
+		for (int i = 0; i < BAD_PLAYERS; i++) {
+			players.add(new BadPlayer(getNewHand(drawPile)));
+		}
+		//		for (int i = 0; i < RANDOM_PLAYERS; i++) {
+		//			players.add(new RandomPlayer(getNewHand(drawPile)));
+		//		}
+		//		for (int i = 0; i < EIGHTS_PLAYERS; i++) {
+		//			players.add(new MindTheEights(getNewHand(drawPile)));
+		//		}
+		//		for (int i = 0; i < HAMPER_PLAYERS; i++) {
+		//			players.add(new HamperLeader(getNewHand(drawPile)));
+		//		}
+		//		for (int i = 0; i < HIGH_PLAYERS; i++) {
+		//			players.add(new DiscardHighPoints(getNewHand(drawPile)));
+		//		}
+		for (int i = 0; i < EXTRA_PLAYERS; i++) {
+			players.add(new ExtraCards(getNewHand(drawPile)));
+		}
+		for (int i = 0; i < players.size(); i++) {
+			System.out.println("[" + players.get(i).getClass() + "] " + "Player " + i + "'s hand: " + players.get(i));
+
 		}
 
 		boolean win = false;
@@ -66,5 +88,13 @@ public class Crazy8Game {
 			// System.out.println("Top of discard pile: " + discardPile.top());
 		}
 		System.out.println("\n--------------------\nWinner is player " + player + "!");
+	}
+
+	private static Card[] getNewHand(Stack<Card> drawPile) {
+		Card[] hand = new Card[5];
+		for (int i = 0; i < 5; i++) {
+			hand[i] = drawPile.pop();
+		}
+		return hand;
 	}
 }
