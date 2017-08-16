3fc14b47c07effd258a8c30763182250282d707c
AzesRD <rajpaldhaliwal@cmail.carleton.ca>
2017-08-15 23:56:16 -0400

    Add files via upload

--- HamperLeader.java
+++ HamperLeader.java
@@ -13,18 +13,7 @@ public class HamperLeader extends Player{
 
 	public HamperLeader(Card[] cards){ super(cards); }
  
-  /* play a card */ 
-	public boolean play(DiscardPile discardPile, Stack<Card> drawPile, ArrayList<Player> players)
-	{
-		discardPile.add(this.hand.remove(0));
-		if(this.hand.size() == 0){
-			return true;
-			}
-		return false;
-	}
-	
-	
-		
+ 
 	// checking to see what player is the leader(has the fewest amount of cards)
 	public Player Leader(){
 		if (players[0].getSizeOfHand() < players[1].getSizeOfHand() && players[0].getSizeOfHand() < players[2].getSizeOfHand() &&
@@ -48,31 +37,27 @@ public class HamperLeader extends Player{
 		
 	}
 	
-		
-	public void HamperLeader(){
-		
-		int indexOfNextPlayer=player.indexOf(players)+1;
-		if(indexOfNextPlayer > 3){
-			indexOfNextPlayer = 0;
-		}
-
-		if(players[indexOfNextPlayer]==Leader()){
-					
+	  /* play a card */ 
+	public boolean play(DiscardPile discardPile, Stack<Card> drawPile, ArrayList<Player> players)
+	{
+		Player nextPlayer = players.get(Crazy8Game.getNextPlayer());
+		int s = this.getSizeOfHand(); /* used to check if the size of hands is still the same after checking if there are power cards in hand
+										if the size of hand is still the same(ie. no card was played), then a valid card will be played */
+		if(nextPlayer == Leader()){
 			int i =0; 
-			boolean b = false;
-			// check each card in hand
+			boolean b = false; // if a card is played b will equal true and program will get out of the loop
+				// check each card in hand
 			while(i<this.getSizeOfHand() && b==false){ 
 				//checking to see each card in hand, if there is a power card in the player's hand, then they will play it
 				if(this.hand.get(i).getRank()==8){
 					discardPile.add(this.hand.remove(i)); //add the 8 card to the discard pile, if true
 					b = true;
-                    
 							
 				}else if(this.hand.get(i).getRank()==4){
 					if(this.hand.get(i).getSuit() == discardPile.top().getSuit()){
 						discardPile.add(this.hand.remove(i));
 						b=true;
-					}
+						}
 					
 				}else if(this.hand.get(i).getRank()==2){
 					if(this.hand.get(i).getSuit() == discardPile.top().getSuit()){
@@ -84,18 +69,29 @@ public class HamperLeader extends Player{
 					if(this.hand.get(i).getSuit() == discardPile.top().getSuit()){
 						discardPile.add(this.hand.remove(i));
 						b=true;
-					}
-				}	
+						}
+				}
 				i=i+1;
-				
+							
+			}
+			
+			if(this.getSizeOfHand()==s){
+			
+				for(int j =0; j<this.getSizeOfHand(); j=j+1){
+					if (discardPile.isValidPlay(this.hand.get(j))==true){
+						discardPile.add(this.hand.remove(j));
+					}
+				}
+			}
+			
+		}else{
+			for(int j =0; j<this.getSizeOfHand(); j=j+1){
+				if (discardPile.isValidPlay(this.hand.get(j))==true){
+					discardPile.add(this.hand.remove(j));
+				}
 			}
 		}
+		return getSizeOfHand()==0;
 	}
-		
+}	
 	
-	
-	
-	
-	
-	
-}
