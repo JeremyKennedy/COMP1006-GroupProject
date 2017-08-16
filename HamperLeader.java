3cf1877859edd0dac989f538ec245dca3e99b391
Jeremy Kennedy <me@jeremyk.net>
2017-08-16 01:15:52 -0400

    added some things to the HamperLeader class: - made it pick up cards when necessary - made it save power cards when previous player is the leader - made it play sevens when previous player is the leader - simplified it using methods like Card.isPowerCard() and DiscardPile.isValidPlay()

--- HamperLeader.java
+++ HamperLeader.java
@@ -1,75 +1,74 @@
 // Author: Rajpal
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Stack;
 
-public class HamperLeader extends Player{
+public class HamperLeader extends Player {
 
 
-	public HamperLeader(Card[] cards){ super(cards); }
+	public HamperLeader(Card[] cards) {
+		super(cards);
+	}
 
-	  /* play a card */
-	public boolean play(DiscardPile discardPile, Stack<Card> drawPile, ArrayList<Player> players)
-	{
-		Player leader = players.get(0);
+	/* play a card */
+	public boolean play(DiscardPile discardPile, Stack<Card> drawPile, ArrayList<Player> players) {
+		// find the size of the leader's hand
+		int nextPlayerSize = players.get(Crazy8Game.getNextPlayer()).getSizeOfHand();
+		int prevPlayerSize = players.get(Crazy8Game.getPrevPlayer()).getSizeOfHand();
+		int leaderSize = players.get(0).getSizeOfHand();
 		for (Player player : players) {
-			if (player.getSizeOfHand() > leader.getSizeOfHand()) {
-				leader = player;
+			if (player.getSizeOfHand() < leaderSize) {
+				leaderSize = player.getSizeOfHand();
 			}
 		}
-		Player nextPlayer = players.get(Crazy8Game.getNextPlayer());
-		int s = this.getSizeOfHand(); /* used to check if the size of hands is still the same after checking if there are power cards in hand
-										if the size of hand is still the same(ie. no card was played), then a valid card will be played */
-		if(nextPlayer == leader){
-			int i =0; 
-			boolean b = false; // if a card is played b will equal true and program will get out of the loop
-				// check each card in hand
-			while(i<this.getSizeOfHand() && b==false){ 
-				//checking to see each card in hand, if there is a power card in the player's hand, then they will play it
-				if(this.hand.get(i).getRank()==8){
-					discardPile.add(this.hand.remove(i)); //add the 8 card to the discard pile, if true
-					b = true;
-							
-				}else if(this.hand.get(i).getRank()==4){
-					if(this.hand.get(i).getSuit() == discardPile.top().getSuit()){
-						discardPile.add(this.hand.remove(i));
-						b=true;
-						}
-					
-				}else if(this.hand.get(i).getRank()==2){
-					if(this.hand.get(i).getSuit() == discardPile.top().getSuit()){
-						discardPile.add(this.hand.remove(i));
-						b=true;
-					}
-				
-				}else if(this.hand.get(i).getRank()==7){
-					if(this.hand.get(i).getSuit() == discardPile.top().getSuit()){
-						discardPile.add(this.hand.remove(i));
-						b=true;
-						}
-				}
-				i=i+1;
-							
-			}
-			
-			if(this.getSizeOfHand()==s){
-			
-				for(int j =0; j<this.getSizeOfHand(); j=j+1){
-					if (discardPile.isValidPlay(this.hand.get(j))==true){
-						discardPile.add(this.hand.remove(j));
-					}
-				}
-			}
-			
-		}else{
-			for(int j =0; j<this.getSizeOfHand(); j=j+1){
-				if (discardPile.isValidPlay(this.hand.get(j))==true){
-					discardPile.add(this.hand.remove(j));
+		// if the next player is the leader, play a power card, if we have one.
+		if (nextPlayerSize == leaderSize) {
+			System.out.println("hampering leader");
+			// iterate through our hand until we find a power card, then play it.
+			for (int i = 0; i < getSizeOfHand(); i++) {
+				// if it is a power card and it can be played, play it
+				if (Card.isPowerCard(hand.get(i)) && discardPile.isValidPlay(hand.get(i))) {
+					return discardCard(hand, i);
 				}
 			}
 		}
-		return getSizeOfHand()==0;
+		// if the previous player is the leader, we try to keep power cards.
+		else if (prevPlayerSize == leaderSize) {
+			System.out.println("holding for leader");
+			// iterate through our hand until either NOT a power card, or a seven, and play it.
+			for (int i = 0; i < getSizeOfHand(); i++) {
+				// if it is a valid play...
+				if (discardPile.isValidPlay(hand.get(i))) {
+					// and either NOT a power card, or IS a seven, play it
+					if (!Card.isPowerCard(hand.get(i)) || hand.get(i).getRank() == 7) {
+						return discardCard(hand, i);
+					}
+				}
+			}
+		}
+		// if we reach this point it either means we couldn't find a power card, or
+		// the next and previous player is not the leader, and we should play normally
+
+		// iterate through our hand until we find a card that can be played, then play it
+		for (int i = 0; i < getSizeOfHand(); i++) {
+			// if it can be played, play it
+			if (discardPile.isValidPlay(hand.get(i))) {
+				return discardCard(hand, i);
+			}
+		}
+		// if we can't find a playable card, pick up cards until we find one
+		while (!drawPile.isEmpty()) {
+			Card pickedCard = pickupCard(drawPile);
+			// if it can be played, play it
+			if (discardPile.isValidPlay(pickedCard)) {
+				return discardCard(hand, getSizeOfHand() - 1);
+			}
+
+		}
+
+		// if we couldn't pick up a playable card, we must pass
+		System.out.println("Passing!");
+		return getSizeOfHand() == 0;
 	}
 }	
 	
